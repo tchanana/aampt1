0be1ed724103805979eb8bec5bd50cc1586ac1f5
AbhijeetSri <AbhijeetSri@users.noreply.github.com>
2014-10-08 15:38:38 -0700

    Update Phase2_1b.m
    Edited to make it a function. It takes 2 file paths as parameters.

--- Phase2_1b.m
+++ Phase2_1b.m
@@ -3,24 +3,25 @@
 % Input two files (state files) and location
 % Output : DTW_sim , DTW Similarity between them
 % make sure to keep file func_DTW.m in the working direcetory
-DIRECTORY = 'C:\MWDB\P2_1B\';
-FILE_NAME_F1 = '1.csv';
-FILE_NAME_F2 = '2.csv';
+% DIRECTORY = 'C:\MWDB\P2_1B\';
+% FILE_NAME_F1 = '1.csv';
+% FILE_NAME_F2 = '2.csv';
+% 
+% prompt = ('Enter File path: ');
+% 
+% DIRECTORY = input(prompt);
+% prompt = ('Enter first File : ');
+% FILE_NAME_F1 = input(prompt);
+% prompt = ('Enter second File : ');
+% FILE_NAME_F2 = input(prompt);
+% 
+% F1_PATH          = strcat(DIRECTORY,FILE_NAME_F1);
+% F2_PATH          = strcat(DIRECTORY,FILE_NAME_F2);
+function DTW_sim = Phase2_1b(F1_PATH,F2_PATH)
 
-prompt = ('Enter File path: ');
-
-DIRECTORY = input(prompt);
-prompt = ('Enter first File : ');
-FILE_NAME_F1 = input(prompt);
-prompt = ('Enter second File : ');
-FILE_NAME_F2 = input(prompt);
-
-F1_PATH          = strcat(DIRECTORY,FILE_NAME_F1);
-F2_PATH          = strcat(DIRECTORY,FILE_NAME_F2);
 F1 = readtable(F1_PATH);
 F2 = readtable(F2_PATH);
 
-% create a distance matrix , as d(i,j) = (F1i - F2i)^2
 no_of_col = numel(F1(1,:)) ;
 sim = zeros();
 for i= 3:no_of_col
@@ -33,4 +34,5 @@ avg = mean(sim(3:end));
 DTW_sim = 1/(1+avg);
 
 display(['similarity: ' num2str(DTW_sim)]);
+end
 
