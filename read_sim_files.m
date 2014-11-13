e5c87830f0e5086f85b5d3c2fa84da069a37d98e
tchanana <chanana.tarun@gmail.com>
2014-11-13 10:31:52 -0700

    Delete read_sim_files.m

--- read_sim_files.m
+++
@@ -1,8 +0,0 @@
-function [ input_file, output_file ] = read_sim_files()
-
-input_file = input('Enter the name of first file with full path::');
-output_file = input('Enter the name of the second file with full parameters::');
-
-
-end
-
