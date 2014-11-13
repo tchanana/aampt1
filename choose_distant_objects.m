2db080e0783088b21be4b85ed5d0db19275f6ae1
tchanana <chanana.tarun@gmail.com>
2014-11-13 10:30:24 -0700

    Delete choose_distant_objects.m

--- choose_distant_objects.m
+++
@@ -1,9 +0,0 @@
-function [ Oa,Ob ] = choose_distant_objects( object_distances )
-
-n = size(object_distances,1);
-Ob = randi(n);
-[~,Oa] = max(object_distances(Ob,:));
-[~,Ob] = max(object_distances(Oa,:));
-
-
-end
